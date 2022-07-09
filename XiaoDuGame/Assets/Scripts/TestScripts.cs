5e78f89b6d93415e913840c8b1ce8bd6eea465d4
Notes <33417990+DLsoul@users.noreply.github.com>
2022-07-09 19:56:38 +0800

    Update TestScripts.cs

--- XiaoDuGame/Assets/Scripts/TestScripts.cs
+++ XiaoDuGame/Assets/Scripts/TestScripts.cs
@@ -7,7 +7,7 @@ public class TestScripts : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		Debug.Log("Ãñ×Ð´óÉ·±Ê");
+		Debug.Log("æ°‘ä»”å¤§ç…žç¬”");
 	}
 
 	// Update is called once per frame
