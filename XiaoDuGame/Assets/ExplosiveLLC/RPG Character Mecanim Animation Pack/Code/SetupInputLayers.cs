d24c707f3873a783f084d4b1925c2ef9e09a7aaa
Notes <33417990+DLsoul@users.noreply.github.com>
2022-07-11 20:31:31 +0800

    动作资源

--- XiaoDuGame/Assets/ExplosiveLLC/RPG Character Mecanim Animation Pack/Code/SetupInputLayers.cs
+++ XiaoDuGame/Assets/ExplosiveLLC/RPG Character Mecanim Animation Pack/Code/SetupInputLayers.cs
@@ -5,12 +5,12 @@ namespace RPGCharacterAnims
 {
 	class SetupInputLayers:AssetPostprocessor
 	{
-		static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-		{
-			SetupMessageWindow window = ( SetupMessageWindow )EditorWindow.GetWindow(typeof(SetupMessageWindow), true, "Load Input and Tag Presets");
-			window.maxSize = new Vector2(330f, 130f);
-			window.minSize = window.maxSize;
-		}
+		//static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+		//{
+		//	SetupMessageWindow window = ( SetupMessageWindow )EditorWindow.GetWindow(typeof(SetupMessageWindow), true, "Load Input and Tag Presets");
+		//	window.maxSize = new Vector2(330f, 130f);
+		//	window.minSize = window.maxSize;
+		//}
 	}
 
 	public class SetupMessageWindow:EditorWindow
